5c1724d83f9504ae233f6ca6ce6d75ef57fc4cc1
Karen <66844562+Karen-et@users.noreply.github.com>
2020-08-10 21:07:52 -0300

    Update login

--- js/login.js
+++ js/login.js
@@ -1,4 +1,5 @@
 
+// Autenticación
 var btn = document.getElementById("btn");
 var user = document.getElementById("user");
 var password = document.getElementById("password");
@@ -8,7 +9,6 @@ var alert = document.getElementById("alert")
 
 function checkData(e){
     if(user.value === "" || password.value === ""){
-        alert.className = "alert alert-warning"
         alert.innerHTML= `<strong>Usuario y/o contraseña incorrectos.</strong>`
         e.preventDefault();
 
@@ -19,5 +19,32 @@ function checkData(e){
 }
 
 btn.addEventListener('click', checkData)
+
+// Efecto del login
+
+document.addEventListener("DOMContentLoaded", event =>{
+
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+
+});
 
 
