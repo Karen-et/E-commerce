123577c38984ce186edb7aa463074da7a2819e3b
Karen-et <66844562+Karen-et@users.noreply.github.com>
2020-08-29 19:13:19 -0300

    Update showName.js

--- js/showName.js
+++ js/showName.js
@@ -3,11 +3,8 @@
   function showName(){
     var user = localStorage.getItem("user");
     var name = document.getElementById("name");
-
-    console.log(user.value);
-  
-  
-     name.innerHTML = user + " &#x2630";
+    
+   name.innerHTML = user + " &#x2630";
   }
 
   showName();
