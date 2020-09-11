a6709ccc3ee16a7ecd324e190bc695f4cf7a3a2a
Karen <66844562+Karen-et@users.noreply.github.com>
2020-09-11 19:30:06 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,24 +1,57 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC3J8NQicfjh3YoC-O9IwSdvLK99WjOqus",
+    authDomain: "e-mercado-4aadd.firebaseapp.com",
+    databaseURL: "https://e-mercado-4aadd.firebaseio.com",
+    projectId: "e-mercado-4aadd",
+    storageBucket: "e-mercado-4aadd.appspot.com",
+    messagingSenderId: "726224731443",
+    appId: "1:726224731443:web:e331e6fb5cedbb880d5246"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const fs = firebase.firestore();
 
-// Validación del login
-var alert = document.getElementById("alert")
+
+// Validación del login con usuario
+const alert = document.getElementById("alert")
+const form = document.getElementById("form_login");
 
 
-function checkData(e){
-    const btn = document.getElementById("btn");
-    const user = document.getElementById("user").value;
-    const pass = document.getElementById("password").value;
+function checkData(){
+    var user = document.getElementById("user").value;
+    var password = document.getElementById("password").value;
 
-    if(!user || !pass){
-        alert.innerHTML= `<strong>Usuario y/o contraseña incorrectos.</strong>`
-    }else{
-        e.preventDefault();
-        // Almacenar los datos en localStorage
-        localStorage.setItem("user", user);
-        window.location.href = "index.html";
+    if(!user || !password){
+        alert.innerHTML= `<strong>Usuario y/o contraseña incorrectos.</strong>`;
+    }
+    else{
+    // Almacenar los datos en localStorage
+    localStorage.setItem("user", user);
+    window.location.href = "index.html";     
     }
 	
 }
 
-btn.addEventListener('click', checkData);
+form.addEventListener('submit', event=>{
+    event.preventDefault();
+    checkData();
+});
+
+//Validación del Login con Google
+const googleButton = document.querySelector("#googleLogin");
+
+googleButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const provider = new firebase.auth.GoogleAuthProvider();
+  auth.signInWithPopup(provider).then((result) => {
+    console.log(result);
+    console.log("google sign in");
+  })
+  .catch(err => {
+    console.log(err);
+  })
+});
 
 
