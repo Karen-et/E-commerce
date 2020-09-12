e3be5e6def6d2d6e04e6cc96772c38ffd34d8e7d
Karen <66844562+Karen-et@users.noreply.github.com>
2020-09-11 23:07:52 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -63,22 +63,15 @@ faceButton.addEventListener("click", event => {
   event.preventDefault();
   const provider = new firebase.auth.FacebookAuthProvider();
   auth.signInWithPopup(provider).then((result) => {
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var faceUser = result.user;
-    localStorage("user", faceUser);
-    window.location.href="index.html";
-    // ...
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
+    //Setear el nombre de usuario de Google como user.
+    var faceUser = result.user.email;
+    console.log(faceUser);
+    localStorage.setItem("user", faceUser)
+    window.location.href = "index.html";
+    })
+  .catch(err => {
+    console.log(err);
+  })
 });
 
 
